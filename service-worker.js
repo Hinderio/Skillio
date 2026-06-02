9a4fc68ef1163ed738b3e47ef2790482d9ed8d97
Hinderio <128827870+Hinderio@users.noreply.github.com>
2026-06-02 12:41:27 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='skillio-static-v2';
-const FILES=['./','index.html','style.css','app.js','manifest.json','assets/icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));});
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
