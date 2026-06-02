bc2b927c7ad3ecbdfd7e1d8735217968a645943a
Hinderio <128827870+Hinderio@users.noreply.github.com>
2026-06-02 20:27:36 +0200

    Update supabase-config.example.js

--- supabase-config.example.js
+++ supabase-config.example.js
@@ -1,4 +1,4 @@
 window.SKILLIO_SUPABASE_CONFIG = Object.freeze({
-  url: '',
-  publicKey: ''
+  url: 'https://spzytdyottsicwmmwsbl.supabase.co/rest/v1/',
+  publicKey: 'sb_publishable_LTDODwxLGIXEkm7jSj6zyg_ujnAZET1'
 });
